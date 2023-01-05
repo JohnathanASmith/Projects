7c41b1dd1eefedde64cd0c97dc7d7900feed3ff2
Johnathan Smith <112116587+JohnathanASmith@users.noreply.github.com>
2023-01-05 15:31:26 -0500

    Delete Current Projects/2022UNCW_Navigator directory

--- Current Projects/2022UNCW_Navigator/lib/constants/app_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppData {
-  const AppData._();
-
-  static AssetImage get logo => const AssetImage('assets/images/logo.jpg');
-  static AssetImage get defaultProfile => const AssetImage('assets/images/default_profile.png');
-
-}
